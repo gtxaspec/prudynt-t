0b21f491fe2d4e6bcd5d89c1c1afe53213f91e6d
gtxaspec <gtxent@gmail.com>
2025-02-09 17:03:30 -0800

    rtspserver: increase session reclamation seconds to 20

--- src/RTSP.cpp
+++ src/RTSP.cpp
@@ -96,11 +96,11 @@ void RTSP::start()
         auth->addUserRecord(
             cfg->rtsp.username,
             cfg->rtsp.password);
-        rtspServer = RTSPServer::createNew(*env, cfg->rtsp.port, auth, 10);
+        rtspServer = RTSPServer::createNew(*env, cfg->rtsp.port, auth, 20);
     }
     else
     {
-        rtspServer = RTSPServer::createNew(*env, cfg->rtsp.port, nullptr, 10);
+        rtspServer = RTSPServer::createNew(*env, cfg->rtsp.port, nullptr, 20);
     }
     if (rtspServer == NULL)
     {
